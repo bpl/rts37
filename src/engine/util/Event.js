189b8891a162ba74ae957dce774192821418b652
Aapo Laitinen <aapo.laitinen@iki.fi>
2011-07-17 01:52:57 +0300

    Add context support for Events

--- src/engine/util/Event.js
+++ src/engine/util/Event.js
@@ -2,19 +2,25 @@
 
 define(function () {
 
+	function Handler(callback, context) {
+		this.callback = callback;
+		this.context = context;
+	}
+
 	function Event() {
-		this.handlers = [];
+		this._handlers = [];
 	}
 
 	// Registers an event handler to this event. This function specified in the
 	// first argument is the handler to be called.
-	Event.prototype.register = function (func) {
-		this.handlers.push(func);
+	Event.prototype.register = function (callback, context) {
+		this._handlers.push(new Handler(callback, context || null));
 	};
 
-	Event.prototype.emit = function () {
-		for (var i = 0; i < this.handlers.length; ++i) {
-			this.handlers[i]();
+	Event.prototype.emit = function (/* ... */) {
+		for (var i = 0; i < this._handlers.length; ++i) {
+			var handler = this._handlers[i];
+			handler.callback.apply(handler.context, arguments);
 		}
 	};
 
